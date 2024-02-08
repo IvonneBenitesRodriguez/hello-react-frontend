75c57b076d0445977f792ad7c7b368f3e1a937ee
Ivonne <ivonnebenites2015@gmail.com>
2024-02-08 17:19:35 -0500

    Update file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import { Routes, Route, Link } from 'react-router-dom';
-// import axios from 'axios';
+
 import Greeting from './components/Greeting';
 import './App.css';
 
 function App() {
-  // const response = axios.get('http://127.0.0.1:3000/api/v1/greetings');
-  // const res = response.data;
-  // console.log(res);
-  // fetch('http://127.0.0.1:3000/api/v1/greetings').then((response) => response.json()).then((data) => console.log(data));
   return (
     <div className="App">
       <h3>Welcome to Greetings</h3>
